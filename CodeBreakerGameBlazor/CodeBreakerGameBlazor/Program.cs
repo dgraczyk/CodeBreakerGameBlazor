cd5c305a2ecc8e5975b80c6c1d2ce775f7b9181e
dgraczyk <dtgraczyk@gmail.com>
2020-01-30 19:41:39 +0100

    Initialize project

---
+++ CodeBreakerGameBlazor/CodeBreakerGameBlazor/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace CodeBreakerGameBlazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}

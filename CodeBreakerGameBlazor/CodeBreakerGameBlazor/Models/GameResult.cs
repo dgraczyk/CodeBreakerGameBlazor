ef4b58d03ec5c74acd71e0bf542f85b35dbb6a58
dgraczyk <dtgraczyk@gmail.com>
2020-02-01 20:02:17 +0100

    Add game logic

---
+++ CodeBreakerGameBlazor/CodeBreakerGameBlazor/Models/GameResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeBreakerGameBlazor.Models
+{
+    public class GameResult
+    {
+        public bool IsFailure { get; set; }
+
+        public int Moves { get; set; }
+
+        public IEnumerable<string> CorrectResult { get; set; }
+
+        public IEnumerable<string> Answer { get; set; }
+
+        public TimeSpan GameTime { get; set; }
+    }
+}
